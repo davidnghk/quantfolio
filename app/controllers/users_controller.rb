6c2c3886648842bb30a6de1ea0ed94e9ec8f4d6e
David Ng <david.ng.hk@gmail.com>
2016-04-12 22:36:25 +0800

    The end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,4 +27,42 @@ class UsersController < ApplicationController
       redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend."
     end
   end
+  
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+
+  def except_current_user(users)
+    users.reject {|user| user.id == self.id}
+  end
+
+  def self.search(param)
+    return User.none if param.blank?
+    
+    param.strip!
+    param.downcase!
+    (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+   matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("lower(#{field_name}) like ?", "%#{param}%")
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.user_stocks
+  end
+  
 end
